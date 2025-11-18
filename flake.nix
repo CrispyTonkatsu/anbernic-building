d4ebf68be61644942adc24129d1bfaaf4b6ba2d4
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-11-17 21:41:16 -0800

    Got the program to work on the funny device. We are so winning

--- flake.nix
+++ flake.nix
@@ -1,45 +1,48 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    utils.url = "github:numtide/flake-utils";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
       self,
-      nixpkgs,
-      utils,
       rust-overlay,
+      nixpkgs,
     }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
 
-        toolchain = [
-          pkgs.rust-bin.fromRustupToolchainFile
-          ./toolchain.toml
-        ];
-
-      in
-      {
-        devShells.${system}.default =
-          with pkgs;
+    # TODO: Make this use the toolchain file for more compatibility
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgsCross = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform;
+          rust-bin = rust-overlay.lib.mkRustBin { } pkgsCross.buildPackages;
+        in
+        pkgsCross.callPackage (
+          {
+            mkShell,
+            pkg-config,
+            qemu,
+            openssl,
+            stdenv,
+          }:
           mkShell {
-            buildInputs = [
-              toolchain
-              cargo
-              rustc
-              rustfmt
-              rustPackages.clippy
+            nativeBuildInputs = [
+              (rust-bin.fromRustupToolchainFile ./toolchain.toml)
+              pkg-config
             ];
 
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
-      }
-    );
+            depsBuildBuild = [ qemu ];
+            buildInputs = [ openssl ];
+
+            env = {
+              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
+              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUNNER = "qemu-aarch64";
+              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+              CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+            };
+          }
+        ) { };
+    };
 }
